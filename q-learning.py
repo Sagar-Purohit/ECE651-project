6a81d0065f6445f7c7acfe51d9e2185b0bd6f8b9
Gayathri Ravada <gdravada@uwaterloo.ca>
2020-03-27 18:12:34 -0400

    unit test for q-learning -gayathri

--- q-learning.py
+++ q-learning.py
@@ -9,6 +9,15 @@ import torch.nn.functional as F
 import torch.optim as optim
 import torch.autograd as autograd
 from torch.autograd import Variable
+from torchtest import assert_vars_same
+import torch_testing as tt
+import unittest
+
+class NaNTensorException(Exception):
+  pass
+
+class InfTensorException(Exception):
+  pass
 
 from base_layer import Network,ReplayMemory
 
@@ -51,6 +60,10 @@ class DeepQNetwork():
 
         self.last_action = 0
         self.last_reward = 0
+        
+        #for testing purpose
+        print('Our list of parameters', [ np[1] for np in self.model.named_parameters() ])
+        self.initial_param = [ np[1] for np in self.model.named_parameters()]
 
     # Decides what the next action should be
     def select_action(self, state):
@@ -88,5 +101,35 @@ class DeepQNetwork():
 
     def score(self):
         return sum(self.reward_average) / (len(self.reward_average) + 1.)
+    
+    #Unit test for q-learning 
+    def test(self):
+        self.after_param = [ np[1] for np in self.model.named_parameters() ]
+        print('After save', self.after_param)
+        #####test for same params
+        count=0
+        for i,a_val in enumerate(self.after_param):
+            if(tt.assert_equal(a_val, self.initial_param[i])== None):
+                print('tensor unit test [', i,']passed')
+                count=count+1
+        if(count==4):
+            print('unit test for Q-learning passed')
+        
+        
+        ###test for Nan
+        for val in self.after_param:
+         try:
+            assert not torch.isnan(val).byte().any()
+         except AssertionError:
+            raise NaNTensorException("There was a NaN value in tensor")
+            
+        ###test for infinite values
+        for val in self.after_param:
+            try:
+                assert torch.isfinite(val).byte().any()
+            except AssertionError:
+                raise InfTensorException("There was an Inf value in tensor")
+        
+        
 
 
